98d5852b1271e70836887d8813b40f58d636a83e
batrick <batrick@batbytes.com>
2010-04-18 00:50:27 +0000

    little better testing
    git-svn-id: file:///cave/home/batrick/scm/lua-signal.svn/trunk@45 c806cf25-6737-4204-a6ff-d454d43a9a0d

--- test.lua
+++ test.lua
@@ -1,5 +1,17 @@
 require "signal"
 
-signal.signal("SIGTERM", function() io.stdout:write("bye!\n") os.exit(-1) end);
+signal.signal("SIGTERM", function(n, i) print("signal handler", n, i); end);
+
+signal.raise("SIGTERM");
+
+signal.signal("SIGTERM", "ignore");
+
+signal.raise("SIGTERM");
+
+print "signal ignored";
+
+signal.signal("SIGTERM", "default");
+
+print "signal default action";
 
 signal.raise("SIGTERM");
